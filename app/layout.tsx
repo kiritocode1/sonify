ef4e3762c38ec4e2a2278a465fdf44857d0d8aac
kiritocode1 <kathawalearyan9@gmail.com>
2025-10-08 12:32:16 +0530

    Stable

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Sonification  by aryank.space",
+  description: "Created with inspiration from lessrain ",
+  generator: "aryank.space",
 }
 
 export default function RootLayout({
